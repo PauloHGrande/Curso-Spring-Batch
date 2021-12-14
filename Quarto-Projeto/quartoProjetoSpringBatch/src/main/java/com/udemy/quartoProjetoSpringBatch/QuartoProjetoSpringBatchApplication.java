7ef25e3f6b074e898b07e95cf52dbddd59c6310e
phg <paulo.grande@matera.com>
2021-12-14 17:18:37 -0300

    Quarto Projeto Refaturando código e Criando Package job,step e tasklet

---
+++ Quarto-Projeto/quartoProjetoSpringBatch/src/main/java/com/udemy/quartoProjetoSpringBatch/QuartoProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.quartoProjetoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartoProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartoProjetoSpringBatchApplication.class, args);
+	}
+
+}
