906e43bfb3aa1bb9330795d5634c9eee1ee726e4
phg <paulo.grande@matera.com>
2021-12-14 16:54:59 -0300

    Terceiro Projeto Chunk com reader,processor e writer

---
+++ Terceiro-Projeto/terceiroProjetoSpringBatch/src/main/java/com/udemy/terceiroProjetoSpringBatch/TerceiroProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.terceiroProjetoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerceiroProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerceiroProjetoSpringBatchApplication.class, args);
+	}
+
+}
