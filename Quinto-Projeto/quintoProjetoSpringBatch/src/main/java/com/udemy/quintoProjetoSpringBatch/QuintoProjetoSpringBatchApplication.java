ebf8421712c03197f0566434980496bcbd144082
phg <paulo.grande@matera.com>
2022-01-03 14:49:20 -0300

    Criando um Arquivo de configuração para multi-bancos

---
+++ Quinto-Projeto/quintoProjetoSpringBatch/src/main/java/com/udemy/quintoProjetoSpringBatch/QuintoProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.quintoProjetoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintoProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintoProjetoSpringBatchApplication.class, args);
+	}
+
+}
